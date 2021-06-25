e7dc5e258ca41469b0d3e7b827dc713f038f8a4a
Hadi Kachmar <HADIKACHMAR8@GMAIL.COM>
2021-06-25 04:17:27 +0300

    First commit

---
+++ lib/grid_search.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+
+class GridSearchScreen extends StatefulWidget {
+  @override
+  _GridSearchScreenState createState() => _GridSearchScreenState();
+}
+
+class _GridSearchScreenState extends State<GridSearchScreen> {
+  List<String> foodList = [
+    'Orange',
+    'Berries',
+    'Lemons',
+    'Apples',
+    'Mangoes',
+    'Dates',
+    'Avocados',
+    'Black Beans',
+    'Chickpeas',
+    'Pinto beans',
+    'White Beans',
+    'Green lentils',
+    'Split Peas',
+    'Rice',
+    'Oats',
+    'Quinoa',
+    'Pasta',
+    'Sparkling water',
+    'Coconut water',
+    'Herbal tea',
+    'Kombucha',
+    'Almonds',
+    'Peannuts',
+    'Chia seeds',
+    'Flax seeds',
+    'Canned tomatoes',
+    'Olive oil',
+    'Broccoli',
+    'Onions',
+    'Garlic',
+    'Carots',
+    'Leafy greens',
+    'Meat',
+  ];
+  List<String>? foodListSearch;
+  final FocusNode _textFocusNode = FocusNode();
+  TextEditingController? _textEditingController = TextEditingController();
+  @override
+  void dispose() {
+    _textFocusNode.dispose();
+    _textEditingController!.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.blue.shade300,
+            title: Container(
+              decoration: BoxDecoration(
+                  color: Colors.blue.shade200,
+                  borderRadius: BorderRadius.circular(20)),
+              child: TextField(
+                controller: _textEditingController,
+                focusNode: _textFocusNode,
+                cursorColor: Colors.black,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    errorBorder: InputBorder.none,
+                    disabledBorder: InputBorder.none,
+                    hintText: 'Search here',
+                    contentPadding: EdgeInsets.all(8)),
+                onChanged: (value) {
+                  setState(() {
+                    foodListSearch = foodList
+                        .where(
+                            (element) => element.contains(value.toLowerCase()))
+                        .toList();
+                    if (_textEditingController!.text.isNotEmpty &&
+                        foodListSearch!.length == 0) {
+                      print('foodListSearch length ${foodListSearch!.length}');
+                    }
+                  });
+                },
+              ),
+            )),
+        body: _textEditingController!.text.isNotEmpty &&
+                foodListSearch!.length == 0
+            ? Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Icon(
+                          Icons.search_off,
+                          size: 160,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'No results found,\nPlease try different keyword',
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            : GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 3,
+                  crossAxisSpacing: 12,
+                ),
+                itemCount: _textEditingController!.text.isNotEmpty
+                    ? foodListSearch!.length
+                    : foodList.length,
+                itemBuilder: (ctx, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          child: Icon(Icons.food_bank),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(_textEditingController!.text.isNotEmpty
+                            ? foodListSearch![index]
+                            : foodList[index]),
+                      ],
+                    ),
+                  );
+                }));
+  }
+}
